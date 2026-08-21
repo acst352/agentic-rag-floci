85cafbe0e349c3c65131f89cf7e941dd6eac8912
AlejandroWare <alejandro.lopez@colimasoft.com>
2026-08-21 11:39:43 -0600

    refactor(lambda demo): eliminar lambda que hacia hola mundo

--- my-lambda/index.js
+++
@@ -1,7 +0,0 @@
-﻿exports.handler = async (event) => {
-  const name = event.name || "Floci";
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: `Hola, ${name}!`, timestamp: new Date().toISOString() })
-  };
-};
